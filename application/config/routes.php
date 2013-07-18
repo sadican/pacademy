6fabec040d607fe2fb7d8ff3df76cc52e1a33587
Mahmud Rasih ÇELENLİOĞLU <sadican09@gmail.com>
2013-07-18 12:34:08 +0300

    static page route is updated for home page

--- application/config/routes.php
+++ application/config/routes.php
@@ -38,7 +38,9 @@
 |
 */
 
-$route['default_controller'] = 'pages/view';
+$route['default_controller'] = 'v_home/view';
+$route['v_home/(:any)'] = 'v_home/view/$1';
+$route['v_home'] = 'v_home';
 $route['(:any)'] = 'pages/view/$1';
 $route['404_override'] = '';
 
