9ace39602c369eaa44c158ac5e6b7d2d01c73ded
Mahmud Rasih ÇELENLİOĞLU <sadican09@gmail.com>
2013-07-18 12:21:09 +0300

    send user name to header

--- application/controllers/pages.php
+++ application/controllers/pages.php
@@ -2,18 +2,17 @@
 
 class Pages extends CI_Controller {
 
-	public function view($page = 'home')
+	public function view($page = 'v_home')
 	{
 		if( !file_exists('application/views/pages/'.$page.'.php'))
 		{
 			show_404();
-			
 		}
 		
-		$data['title'] = ucfirst($page); // Capitalize the first letter
+		$data['user_name'] = $this->dictionary->getSingleInfo('user_name')->dict_value;
 		
 		$this->load->view('templates/header', $data);
-		$this->load->view('pages/'.$page, $data);
-		$this->load->view('templates/footer', $data);
+		$this->load->view('pages/'.$page);
+		$this->load->view('templates/footer');
 	}
 }
