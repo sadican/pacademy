fd16ed0342997fed4d52444817c11ae3ab1512cb
Mahmud Rasih ÇELENLİOĞLU <sadican09@gmail.com>
2013-07-18 12:33:43 +0300

    view function is updated

--- application/controllers/c_home.php
+++ application/controllers/c_home.php
@@ -1,32 +1,28 @@
 <?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
 
 class C_Home extends CI_Controller {
-
+	
+	public function __construct()
+	{
+		parent::__construct();
+		$this->load->model('m_home');
+	}
+	
 	public function index()
-	{		
-		$this->main();
+	{
+		$data = $this->m_home->getUserInfo();
+		
+		$this->load->view('templates/header', $data);
+		$this->load->view('pages/v_home', $data);
+		$this->load->view('templates/footer');
 	}
 
-	public function main()
+	public function view()
 	{
-		$this->load->model("dictionary");
+		$data = $this->m_home->getUserInfo();
 		
-		$data['user_name'] = $this->dictionary->getSingleInfo('user_name')->dict_value;
-		$data['user_surname'] = $this->dictionary->getSingleInfo('user_surname')->dict_value;
-		$data['user_position'] = $this->dictionary->getSingleInfo('user_position')->dict_value;
-		$data['user_department'] = $this->dictionary->getSingleInfo('user_department')->dict_value;
-		$data['user_mail'] = $this->dictionary->getSingleInfo('user_mail')->dict_value;
-		$data['user_work_address'] = $this->dictionary->getSingleInfo('user_work_address')->dict_value;
-		$data['user_work_phone'] = $this->dictionary->getSingleInfo('user_work_phone')->dict_value;
-		$data['user_work_room'] = $this->dictionary->getSingleInfo('user_work_room')->dict_value;
-		$data['user_work_location'] = $this->dictionary->getSingleInfo('user_work_location')->dict_value;
-		$data['user_university'] = $this->dictionary->getSingleInfo('user_university')->dict_value;
-		$data['user_research_areas'] = $this->dictionary->getSingleInfo('user_research_areas')->dict_value;
-		$data['user_education'] = $this->dictionary->getSingleInfo('user_education')->dict_value;
-		$data['user_quote'] = $this->dictionary->getSingleInfo('user_quote')->dict_value;
-		
-		//print($data['user_name']);
-		
-		$this->load->view('v_home', $data);		
+		$this->load->view('templates/header', $data);
+		$this->load->view('pages/v_home', $data);
+		$this->load->view('templates/footer');
 	}
 }
