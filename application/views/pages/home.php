7b57fe32fa5a6294c48f1488561c9a3540afcc5c
Mahmud Rasih ÇELENLİOĞLU <sadican09@gmail.com>
2013-07-20 01:33:50 +0300

    remove old home page

--- application/views/pages/home.php
+++
@@ -1,5 +0,0 @@
-<div class="hero-unit bglightgrey">
-	<h2 class="text-info text-center">TEST</h2>
-</div>
-</div>
-<script src="<?= site_url() ?>assets/js/bootstrap.js" type="text/javascript"></script>
