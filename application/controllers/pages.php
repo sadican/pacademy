51e93a344119fe180c9e6ca22c3cf4e8addc3c5b
Mahmud Rasih ÇELENLİOĞLU <sadican09@gmail.com>
2013-07-20 01:35:57 +0300

    update pages controller

--- application/controllers/pages.php
+++ application/controllers/pages.php
@@ -1,18 +1,55 @@
-<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+<?php
+if (!defined('BASEPATH'))
+	exit('No direct script access allowed');
 
 class Pages extends CI_Controller {
 
-	public function view($page = 'v_home')
-	{
-		if( !file_exists('application/views/pages/'.$page.'.php'))
-		{
-			show_404();
-		}
-		
-		$data['user_name'] = $this->dictionary->getSingleInfo('user_name')->dict_value;
-		
-		$this->load->view('templates/header', $data);
-		$this->load->view('pages/'.$page);
-		$this->load->view('templates/footer');
+	public function __construct() {
+		parent::__construct();
 	}
+
+	public function index($page = 'v_home') {
+		if (!file_exists('application/views/pages/' . $page . '.php')) {
+			show_404();
+		} else if ($page === 'v_home') {
+			$this -> load -> model('m_home');
+
+			$data = $this -> m_home -> getUserProfile();
+
+			$this -> load -> view('templates/header', $data);
+			$this -> load -> view('pages/' . $page, $data);
+			$this -> load -> view('templates/footer');
+		} else {
+			$this -> load -> model('dictionary');
+
+			$data['user_name'] = $this -> dictionary -> getSingleInfo('user_name') -> dict_value;
+
+			$this -> load -> view('templates/header', $data);
+			$this -> load -> view('pages/' . $page, $data);
+			$this -> load -> view('templates/footer');
+		}
+	}
+
+	public function view($page = 'v_home') {
+		if (!file_exists('application/views/pages/' . $page . '.php')) {
+			show_404();
+		} else if ($page === 'v_home') {
+			$this -> load -> model('m_home');
+
+			$data = $this -> m_home -> getUserProfile();
+
+			$this -> load -> view('templates/header', $data);
+			$this -> load -> view('pages/' . $page, $data);
+			$this -> load -> view('templates/footer');
+		} else {
+			$this -> load -> model('dictionary');
+
+			$data['user_name'] = $this -> dictionary -> getSingleInfo('user_name') -> dict_value;
+
+			$this -> load -> view('templates/header', $data);
+			$this -> load -> view('pages/' . $page, $data);
+			$this -> load -> view('templates/footer');
+		}
+	}
+
 }
