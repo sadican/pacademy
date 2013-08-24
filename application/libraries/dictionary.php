b001a31531e2aa1762eb9420e87e8bff34cfef20
Mahmud Rasih ÇELENLİOĞLU <sadican09@gmail.com>
2013-08-24 12:00:58 +0300

    add dictionary library

---
+++ application/libraries/dictionary.php
@@ -0,0 +1,47 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+class Dictionary {
+	
+	private $tableName;
+	private $CI;// = &get_instance();
+
+	public function __construct()
+	{
+		$this->tableName = "dictionary";
+		$this->CI = &get_instance();	
+	}
+	
+	public function getAll()
+	{
+		/* kolonlari virgulle ayirarak yaziyorsun */
+		$this->CI->db->select("dict_key, dict_value");
+		/* hangi tablodan aldigin */
+		$this->CI->db->from($this->tableName);
+		/* where kismi*/
+		//$this->db->where('key =','name');
+		/* sonuclari limitle */
+		//$this->db->limit(2);
+		/* sorguyu calistir */
+		$query = $this->CI->db->get();
+		/* ekrana queryi bastir */
+		//echo $this->db->last_query();
+		/* sonucu goster */
+		//print_r($query->result());
+		/* sonucu dondur */
+		//return $query->result();
+		return $query->result_array();
+	}
+	
+	public function getEntry($info)
+	{
+		$this->CI->db->select("dict_value");
+		$this->CI->db->from($this->tableName);
+		$this->CI->db->where('dict_key =', $info);
+		$this->CI->db->limit(1);
+		
+		$query = $this->CI->db->get();
+
+		return $query->row()->dict_value;
+	}
+}
+?>
