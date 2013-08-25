b86d42ef70d43d7c7dd397c0aee4b0d10f967345
Mahmud Rasih ÇELENLİOĞLU <sadican09@gmail.com>
2013-08-26 01:09:48 +0300

    update header variables

--- application/views/templates/header.php
+++ application/views/templates/header.php
@@ -2,17 +2,18 @@
 <html lang="tr">
 <head>
 	<meta charset="utf-8">
-	<title><?php echo $name . ' ' . $surname ?></title>
+	<title><?php echo $head_title_name ?></title>
+	<link rel="icon" href="<?= site_url() ?>assets/img/favicon.ico">
 	<link rel="stylesheet" href="<?= site_url() ?>assets/css/bootstrap.css" type="text/css" />
     <link rel="stylesheet" href="<?= site_url() ?>assets/css/custom.css" type="text/css" />
 </head>
 <body style="background: url(<?= site_url() ?>assets/img/bg.jpg) repeat;">
     <div class="navbar navbar-static-top">
        <div class="navbar-inner centerMe">
-            <a class="brand"><?php echo $header_name ?></a>
+            <a class="brand"><?php echo $menu_brand_name ?></a>
             <ul class="nav">
             	<li><a href="<?= site_url() ?>">Anasayfa</a></li>
-                <li><a href="education">Eğitim</a></li>
+                <li><a href="profile">Profil</a></li>
             </ul>
         </div>
     </div>
