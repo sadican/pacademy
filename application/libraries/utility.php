bf4f3ef03ac84938bb0a7ca545268f94d62aeefc
Mahmud Rasih ÇELENLİOĞLU <sadican09@gmail.com>
2013-08-26 01:08:15 +0300

    update error trigger functions

--- application/libraries/utility.php
+++ application/libraries/utility.php
@@ -14,22 +14,23 @@ class Utility {
 		if((strcasecmp(gettype($tempData), "array") == 0) 
 			&& (strcasecmp(gettype($realData), "array") == 0) )
 		{
+			//echo "temp:array & data:array </br></br>";
 			$realData = array_merge($tempData, $realData);
 		}
 
 		else if ((strcasecmp(gettype($tempData), "object") == 0)
 			&& (strcasecmp(gettype($realData), "array") == 0) )
 		{
+			//echo "temp:object & data:array </br></br>";
 			foreach ($tempData as $key => $value)
 			{
 				$realData[$key] = $value;
 			}
 		}
-
+		
 		else
 		{
-			echo "LIBRARY(utility) FUNCTION(mergeData) EXPLANATION: if else statement failed!";
-			return null;
+			trigger_error("LIBRARY(utility) FUNCTION(mergeData) EXPLANATION: if else statement failed!", E_USER_ERROR);
 		}
 
 		return $realData;
@@ -46,6 +47,53 @@ class Utility {
 			echo "</br>";
 		}
 		
+	}
+
+	public function printArray($data)
+	{
+		foreach ($data as $key => $value) {
+			if(strcasecmp(gettype($value), "object") == 0)
+			{
+				echo "KEY: ". $key ."</br>".
+				"TYPE: ". gettype($value) ."</br>".
+				"VALUE: ";
+				//print_r($value);
+				
+				foreach ($value as $keyInnerData => $valueInnerData) {
+					echo "</br></br>KEY: ". $keyInnerData ."</br>".
+					"TYPE: ". gettype($valueInnerData) ."</br>".
+					"VALUE: ";
+					print_r($valueInnerData);
+				}
+				
+				echo "</br></br>";
+			}
+			else if(strcasecmp(gettype($value), "array") == 0)
+			{
+				foreach ($value as $keyInnerData => $valueInnerData) {
+					echo "</br></br>KEY: ". $keyInnerData ."</br>".
+					"TYPE: ". gettype($valueInnerData) ."</br>".
+					"VALUE: ";
+					print_r($valueInnerData);
+				}
+			}
+
+			else
+			{
+				echo "KEY: ". $key ."</br>".
+				"TYPE: ". gettype($value) ."</br>".
+				"VALUE: ". $value ."</br></br>";
+			}			
+		}
+	}
+
+	public function convertToAssociativeArray($tempData, $realData)
+	{
+		foreach ($tempData as $key => $value) {
+			$realData[$key] = $value;
+		}
+
+		return $realData;
 	}
 }
 ?>
